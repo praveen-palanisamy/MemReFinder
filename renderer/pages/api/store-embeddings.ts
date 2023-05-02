8c56383cbd43e6252c47b36ea645535bec08b9dc
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2023-05-01 23:21:05 -0700

    Add store-embeddings api (missed in prev commit)

---
+++ renderer/pages/api/store-embeddings.ts
@@ -0,0 +1,42 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {supabase } from "../../lib/supabaseClient";
+import {FileLite } from "../../types/file";
+
+
+const storeEmbeddings = async (fileObject: FileLite) => {
+
+    const {name, url, type, size, extractedText, embedding, chunks} = fileObject
+
+    const {data, error} = await supabase.from('File').insert({
+       name,
+       url,
+       type,
+       size,
+       extractedText,
+       meanEmbedding: embedding,
+    }).select('id')
+
+    const file_id = data[0].id
+
+    console.log("File stored with file_id:", file_id)
+    console.log("error:", error)
+
+    const {error: error2} = await supabase.from('TextEmbedding').insert(chunks.map((chunk) => ({
+        text: chunk.text,
+        embedding: chunk.embedding,
+        fileId: file_id
+    })))
+    console.log("Embeddings stored")
+    console.log("Error:", error2)
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== "POST") {
+        res.status(405).json({ error: `HTTP method ${req.method} not allowed` });
+        return;
+    }
+
+    const fileObject = req.body
+    await storeEmbeddings(fileObject)
+    res.status(200).json({message: "Embeddings stored"})
+}
