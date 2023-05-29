10f7c206b7d9f6a5a86879d67b89545a93c5f420
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2023-05-28 21:28:40 -0700

    Add thin left-side-bar with UserSettings at the bottom-left

--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -12,40 +12,41 @@ export default function FileQandA() {
   const { data: session, status } = useSession();
   const isLoadingUser = status === "loading";
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-screen">
       <Head>
         <title>MemReFinder</title>
       </Head>
-      <div className="text-gray-800">
-        <h1 className="text-center text-4xl">
-          MemReFinder: Chat with your Data
-        </h1>
+        <div className="w-12 bg-gray-100"></div>
+        <div className="w-full">
+          <h1 className="text-center text-4xl">
+            MemReFinder: Chat with your Data
+          </h1>
 
-        <div className="">
-          <center> Chat with your Documents and Files to find Answers.</center>
-          <br />
-        </div>
-        {!isLoadingUser && (
-          <div className="grid grid-flow-row-dense grid-cols-3 grid-gap">
-            <div className="col-span-1 text-center">
-              {" "}
-              1. Load your files:
-              <FileUploadArea
-                handleSetFiles={setFiles}
-                maxNumFiles={100}
-                maxFileSizeMB={25}
-                session={session}
-              />
-              <div className="absolute bottom-0 left-0">
-                <UserSettings session={session} />
+          <div className="">
+            <center> Chat with your Documents and Files to find Answers.</center>
+            <br />
+          </div>
+          {!isLoadingUser && (
+            <div className="grid grid-flow-row-dense grid-cols-3 grid-gap">
+              <div className="col-span-1 text-center">
+                {" "}
+                1. Load your files:
+                <FileUploadArea
+                  handleSetFiles={setFiles}
+                  maxNumFiles={100}
+                  maxFileSizeMB={25}
+                  session={session}
+                />
+              </div>
+              <div className="col-span-2 text-center">
+                <FileQandAArea files={files} />
               </div>
             </div>
-            <div className="col-span-2 text-center">
-              <FileQandAArea files={files} />
-            </div>
-          </div>
-        )}
-      </div>
+          )}
+        </div>
+        <div className="absolute bottom-0 left-0">
+          <UserSettings session={session} />
+        </div>
     </div>
   );
 }
