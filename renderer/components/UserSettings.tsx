8e66c5158240e1294f9fe04449e5ee0daecb92b6
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2023-08-20 11:19:31 -0700

    Use headlessui Popover instead of Menu

--- renderer/components/UserSettings.tsx
+++ renderer/components/UserSettings.tsx
@@ -1,8 +1,8 @@
-import { Menu, Transition } from "@headlessui/react";
 import { Fragment, useState, useCallback } from "react";
 import { Cog8ToothIcon } from "@heroicons/react/24/solid";
 import { AuthModal } from "./AuthModal";
 import { signOut } from "next-auth/react";
+import { Popover, Transition } from "@headlessui/react";
 
 // User settings menu with menu items to Login (if not logged in) or Logout (if logged in). Show cog icon if not logged in, else show user icon.
 export default function UserSettings({ session, theme, toggleTheme }) {
@@ -19,69 +19,70 @@ export default function UserSettings({ session, theme, toggleTheme }) {
   const inactiveClass = "bg-gray-100 dark:bg-gray-300";
   return (
     <>
-      <Menu as="div" className="ml-1 relative">
+      <Popover>
         {({ open }) => (
           <>
-            <div>
-              <Menu.Button className="bg-white dark:bg-slate rounded-full flex text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
-                <span className="sr-only">Open user menu</span>
-                <Cog8ToothIcon className="h-8 w-8" />
-              </Menu.Button>
-            </div>
+            <Popover.Button
+              className={`${
+                open ? "text-gray-900" : "text-gray-500"
+              } group bg-white dark:bg-slate rounded-full inline-flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500`}
+            >
+              <span className="sr-only">Open user menu</span>
+              <Cog8ToothIcon className="h-8 w-8" />
+            </Popover.Button>
+
             <Transition
               show={open}
               as={Fragment}
-              enter="transition ease-out duration-100"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95"
+              enter="transition ease-out duration-200"
+              enterFrom="opacity-0 translate-y-1"
+              enterTo="opacity-100 translate-y-0"
+              leave="transition ease-in duration-150"
+              leaveFrom="opacity-100 translate-y-0"
+              leaveTo="opacity-0 translate-y-1"
             >
-              <Menu.Items
+              <Popover.Panel
                 static
-                className="origin-top-right transform -translate-y-1 mt-2 w-36 rounded-md shadow-lg py-1 bg-white dark:bg-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none"
+                className="origin-top-right absolute z-10 right-0 mt-2 w-36 rounded-md shadow-lg bg-white dark:bg-gray-300 ring-1 ring-black ring-opacity-5 focus:outline-none"
               >
-                <Menu.Item>
-                  {!user
-                    ? ({ active }) => (
-                        <button
-                          onClick={() => setAuthModalOpen(true)}
-                          className={`${
-                            active ? `${activeClass}` : `${inactiveClass}`
-                          } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
-                        >
-                          Login
-                        </button>
-                      )
-                    : ({ active }) => (
-                        <button
-                          onClick={() => signOut()}
-                          className={`${
-                            active ? `${activeClass}` : `${inactiveClass}`
-                          } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
-                        >
-                          Logout
-                        </button>
-                      )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
+                <div className="py-1">
+                  <Popover.Button>
+                    {!user ? (
+                      <button
+                        onClick={() => setAuthModalOpen(true)}
+                        className={`${
+                          open ? `${activeClass}` : `${inactiveClass}`
+                        } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                      >
+                        Login
+                      </button>
+                    ) : (
+                      <button
+                        onClick={() => signOut()}
+                        className={`${
+                          open ? `${activeClass}` : `${inactiveClass}`
+                        } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
+                      >
+                        Logout
+                      </button>
+                    )}
+                  </Popover.Button>
+                  <Popover.Button>
                     <button
                       onClick={handleThemeToggle}
                       className={`${
-                        active ? `${activeClass}` : `${inactiveClass}`
+                        open ? `${activeClass}` : `${inactiveClass}`
                       } block px-4 py-2 text-sm text-gray-700 w-full text-left`}
                     >
                       {theme === "light" ? "Dark" : "Light"} theme
                     </button>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
+                  </Popover.Button>
+                </div>
+              </Popover.Panel>
             </Transition>
           </>
         )}
-      </Menu>
+      </Popover>
       <AuthModal {...authModalProps} />
     </>
   );
