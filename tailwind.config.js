819fc1b94ac761c8f47b1c9a1d5d478cd11fe355
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2023-08-20 19:09:09 -0700

    Add daisyui for leveraging pre-built tailwind components

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,31 @@ module.exports = {
   theme: {
     extend: {},
   },
-  darkMode: 'class',
+  darkMode: "class",
   keyframes: {
     blink: {
       "0%, 100%": { opacity: 1 },
       "50%": { opacity: 0 },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      {
+        mytheme: {
+          primary: "#fbbf24",
+          secondary: "#b5e57e",
+          accent: "#d6b251",
+          neutral: "#322541",
+          "base-100": "#1f2937",
+          info: "#51c2ec",
+          success: "#25ad94",
+          warning: "#f1c241",
+          error: "#f24131",
+        },
+      },
+    ],
+  },
 };
