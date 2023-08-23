a9ddabd243a35a4b9e2b63f7097435bb831bc0e3
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2023-08-22 18:47:31 -0700

    Update Q&A elem alignments

--- renderer/components/FileQandA.tsx
+++ renderer/components/FileQandA.tsx
@@ -10,7 +10,7 @@ export default function FileQandA() {
   const { data: session, status } = useSession();
   const isLoadingUser = status === "loading";
   return (
-    <div className="flex flex-col justify-center align-center min-h-screen">
+    <div className="flex flex-col justify-center align-center">
       {!isLoadingUser && (
         <div
           className={`grid grid-flow-row-dense grid-cols-3 grid-gap bg-white dark:bg-slate-800`}
--- renderer/pages/index.tsx
+++ renderer/pages/index.tsx
@@ -7,7 +7,7 @@ export default function Main() {
   const { mode } = useContext(AppContext);
   const supportedModes = ["", "cloud"];
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="">
       {mode === "cloud" && <FileQandA />}
       {mode === "" && <WelcomePage />}
       {!supportedModes.includes(mode) && <WelcomePage />}
